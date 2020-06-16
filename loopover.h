c91157ee84344a4220bc1679c38787d9aea558d4
Luc de wit <luc.dewit@hotmail.nl>
2020-06-16 19:26:08 +0200

    worked on fist layer insertion

--- loopover.h
+++ loopover.h
@@ -12,7 +12,7 @@ typedef char bool;
 
 typedef struct {
     size_t size;
-    int ** board;
+    int* board;
 } board;
 
 typedef struct {
@@ -22,22 +22,24 @@ typedef struct {
 } move;
 
 void initLoopEngine() {
-    srand(time(0)*999999999999999); // intialize random number generator
+    srand(time(NULL)); // intialize random number generator
 }
 
 board createBoard(size_t size) {
     // initialize board
     board b;
     b.size = size;
-    b.board = calloc(size, sizeof(int * ));
+
+    int* board = (int*) malloc(size * size * sizeof(int));
+    
+    b.board = board;
 
     //fill board
+    int count = 0;
     for (int i = 0; i < size; i++) {
-        int * row = calloc(size, sizeof(int));
         for (int j = 0; j < size; j++) {
-            row[j] = i * size + j + 1;
+            *(b.board+i*size+j) = ++count;
         }
-        b.board[i] = row;
     }
 
     return b;
@@ -46,7 +48,7 @@ board createBoard(size_t size) {
 void printBoard(board B) {
     for (int i = 0; i < B.size; i++) {
         for (int j = 0; j < B.size; j++) {
-            int cell = B.board[i][j];
+            int cell = *(B.board+i*B.size+j);
             printf("%d ", cell);
 
             if (B.size > 3 && cell < 10) {
@@ -99,37 +101,47 @@ move getRandomMove(size_t maxCount) {
     return rmove;
 }
 
+void setBoard(board* B, unsigned int x, unsigned int y, unsigned int val) {
+    B->board[y * B->size + x] = val;
+}
+
+unsigned int getBoard(board* B, unsigned int x, unsigned int y) {
+
+}
+
 void moveHorizontal(board* B, unsigned int row, bool toTheRight) {
     if (toTheRight) { // move to the right
-        int temp = B -> board[row - 1][B -> size - 1];
-        for (int i = B -> size - 1; i > 0; i--)
-            B -> board[row - 1][i] = B -> board[row - 1][i - 1];
-        B -> board[row - 1][0] = temp;
+        int temp = B->board[(row - 1)  * B->size + B->size - 1];
+        for (int i = B->size - 1; i > 0; i--)
+            B->board[(row - 1) * B->size + i] = B->board[(row - 1) * B->size + i - 1];
+        setBoard(B, 0, row-1, temp);
     }
 
     else { //move to the left
-        int temp = B -> board[row - 1][0];
+        int temp = B->board[(row - 1)  * B -> size];
         for (int i = 0; i < B -> size - 1; i++)
-            B -> board[row - 1][i] = B -> board[row - 1][i + 1];
-        B -> board[row - 1][B -> size - 1] = temp;
+            B->board[(row - 1) * B->size + i] = B->board[(row - 1) * B->size + i+1];
+        setBoard(B, B->size-1, row-1, temp);
     }
 }
+
 
 void moveVertical(board* B, unsigned int col, bool up) {
     if (up) { // move up
-        int temp = B -> board[0][col - 1];
-        for (int i = 0; i < B -> size - 1; i++)
-            B -> board[i][col - 1] = B -> board[i + 1][col - 1];
-        B -> board[B -> size - 1][col - 1] = temp;
+        int temp = B->board[col - 1];
+        for (int i = 0; i < B->size - 1; i++)
+            B->board[i * B->size + (col - 1)] = B->board[(i+1) * B->size + (col - 1)];
+        setBoard(B, col - 1, B->size - 1, temp);
     }
 
     else { //move down
-        int temp = B -> board[B -> size - 1][col - 1];
+        int temp = B->board[(col - 1) + (B->size-1)*B->size];
         for (int i = B -> size - 1; i > 0; i--)
-            B -> board[i][col - 1] = B -> board[i - 1][col - 1];
-        B -> board[0][col - 1] = temp;
+            B->board[i * B->size + col - 1] = B->board[(i-1) * B->size + col - 1];
+        setBoard(B, col-1, 0, temp);
     }
 }
+
 
 void applyMove(board* B, move M) {
     if (M.isRow) {
@@ -156,28 +168,59 @@ typedef struct{
     unsigned int y;
 } location;
 
+
+
+
 location locateNumber(unsigned int number, board* B){
     location ret;
-    for (unsigned int y = 0; y < B->size; y++){
-        for (unsigned int x = 0; x < B->size; x++){
-            if (B->board[y][x] == number){
-                ret.x = x;
-                ret.y = y;
-                return ret;
-            }
+    for (unsigned int i = 0; i < B->size*B->size; i++){
+        if (B->board[i] == number){
+            ret.x = i % B->size;
+            ret.y = ceil(i/B->size);
+            return ret;
         }
     }
 
+
     printf("error, cant locate number %d", number);
+    puts("");
+    printBoard(*B);
     exit(1);
 }
 
-void solveInsertion(board* B) {
-    puts("solving first row");
-    location tile = locateNumber(1, B);
+unsigned int solveInsertion(board* B) {
+    unsigned int moveCount = 0;
 
-    printf("tile one is at x = %d y = %d", tile.x, tile.y);
+    // first row
+    for (unsigned int tileNum = 1; tileNum < B->size; tileNum++) {
+        location tile = locateNumber(tileNum, B);
+
+        // move downward if already in row 1
+        if (tileNum != 1 && tile.y == 0 && tile.x != tileNum-1) {
+            moveVertical(B, tile.x+1, False);
+            tile = locateNumber(tileNum, B);
+        }
+        
+        // skip if already in place
+        if (tile.y == 0 && tile.x == tileNum-1) {
+            continue;
+        }
+
+        // move to the right x position
+        while(tile.x != tileNum-1) {
+            moveHorizontal(B, tile.y+1, (tile.x-tileNum-1) % B->size < B->size/2 ? False : True);
+            tile = locateNumber(tileNum, B);
+            moveCount++;
+        }
+
+        // move up or down to row 1
+        while(tile.y != 0) {
+            moveVertical(B, tile.x+1, tile.y > floor(B->size/2) ? False : True);
+            tile = locateNumber(tileNum, B);
+            moveCount++;
+
+        }
+    }
+    
+    return moveCount;
 }
-
-void solveWillems();
-void solveImperial();
--- main.c
+++ main.c
@@ -3,11 +3,17 @@
 int main() {
     initLoopEngine();
 
-    board B = createBoard(5);
-    scrambleRandomMoves(&B, 1000);
-    solveInsertion(&B);
-    puts("\n");
-    printBoard(B);
-
+    board B = createBoard(2);
+    unsigned int avg = 0;
+    unsigned int solvecount = 500;
+    for (unsigned int i = 0; i < solvecount; i++){
+	    scrambleRandomMoves(&B, 1000);
+	    unsigned int movecount = solveInsertion(&B);
+	    //printBoard(B);
+	    printf("solve %d took %d moves\n", i+1, movecount);
+	    avg += movecount;
+	}
+	printf("avg moves: %f moves\n", (double)avg / solvecount);
+	
     return 0;
 }
